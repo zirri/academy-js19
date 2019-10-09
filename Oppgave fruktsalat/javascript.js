aad43ba1f2c0643ceb21e3e0847fdcab64eaad94
zirri <as.borander@gmail.com>
2019-10-09 11:05:43 +0200

    errorhandling, no valid price, same prices

--- Oppgave fruktsalat/javascript.js
+++ Oppgave fruktsalat/javascript.js
@@ -8,22 +8,27 @@ function makeFruitObject (fruitName, fruitPrice){
 
 function compareFruits(){
     let fruits = [];
+    let messageToUser = '';
     //lage en while-loop i stedet?
     for (let i = 1; i<=5; i++){ 
         if(document.getElementById('fruit'+i).value != ''){
             if(document.getElementById('fruit'+i+'Price').value > 0){
                 fruits.push(makeFruitObject(document.getElementById('fruit'+i).value,document.getElementById('fruit'+i+'Price').value));
             }else{
-                alert('Frukt uten pris blir ikke evaluert');
+                messageToUser = 'Frukt uten gyldig pris er ikke evaluert';
             }
         }
     }  
 
     fruits.sort(function (a, b) {
+        if (a.price === b.price){
+            messageToUser = messageToUser.concat('\n Flere frukter har samme pris. Kan være flere som er billigst/dyrest i tillegg til de som vises.')
+        }
         return a.price - b.price;
     });
 
     alert(`Dyreste ingrediens er: ${fruits[fruits.length-1].name}, som koster ${fruits[fruits.length-1].price}
-        \n Billigste ingrediens er: ${fruits[0].name}, som koster ${fruits[0].price}`);
+        \n Billigste ingrediens er: ${fruits[0].name}, som koster ${fruits[0].price}
+        \n ${messageToUser}`);
 return false;   
 };
