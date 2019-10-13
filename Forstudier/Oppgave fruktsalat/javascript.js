f6326685fd97d63c1ed3698cd1e86548c0687f82
Anne-Siri Borander <as.borander@gmail.com>
2019-10-13 21:13:17 +0200

    tilbakemelding inline på siden (fra alert)

--- Forstudier/Oppgave fruktsalat/javascript.js
+++ Forstudier/Oppgave fruktsalat/javascript.js
@@ -1,4 +1,4 @@
-function makeFruitObject (fruitName, fruitPrice){
+function makeFruitObject(fruitName, fruitPrice) {
     let fruit = {
         name: fruitName,
         price: fruitPrice
@@ -6,29 +6,31 @@ function makeFruitObject (fruitName, fruitPrice){
     return fruit
 };
 
-function compareFruits(){
+function compareFruits() {
     let fruits = [];
     let messageToUser = '';
     //lage en while-loop i stedet?
-    for (let i = 1; i<=5; i++){ 
-        if(document.getElementById('fruit'+i).value != ''){
-            if(document.getElementById('fruit'+i+'Price').value > 0){
-                fruits.push(makeFruitObject(document.getElementById('fruit'+i).value,document.getElementById('fruit'+i+'Price').value));
-            }else{
+    for (let i = 1; i <= 5; i++) {
+        if (document.getElementById('fruit' + i).value != '') {
+            if (document.getElementById('fruit' + i + 'Price').value > 0) {
+                fruits.push(makeFruitObject(document.getElementById('fruit' + i).value, document.getElementById('fruit' + i + 'Price').value));
+            } else {
                 messageToUser = 'Frukt uten gyldig pris er ikke evaluert';
             }
         }
-    }  
+    }
 
-    fruits.sort(function (a, b) {
-        if (a.price === b.price){
+    fruits.sort(function(a, b) {
+        if (a.price === b.price) {
             messageToUser = messageToUser.concat('\n Flere frukter har samme pris. Kan være flere som er billigst/dyrest i tillegg til de som vises.')
         }
         return a.price - b.price;
     });
 
-    alert(`Dyreste ingrediens er: ${fruits[fruits.length-1].name}, som koster ${fruits[fruits.length-1].price}
-        \n Billigste ingrediens er: ${fruits[0].name}, som koster ${fruits[0].price}
-        \n ${messageToUser}`);
-return false;   
+    returnMessage = `Dyreste ingrediens er: ${fruits[fruits.length-1].name}, som koster ${fruits[fruits.length-1].price}
+        <br> Billigste ingrediens er: ${fruits[0].name}, som koster ${fruits[0].price}
+        <br><br> ${messageToUser}`;
+
+    document.getElementById('resultEvaluation').innerHTML = returnMessage;
+    return false
 };
