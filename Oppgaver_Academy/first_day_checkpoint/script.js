73da96fe2bfa7ac2f1bf476dc45ae38478741f34
zirri <as.borander@gmail.com>
2019-10-14 12:47:53 +0200

    endret til liste, function funker, enkel css

--- Oppgaver_Academy/first_day_checkpoint/script.js
+++ Oppgaver_Academy/first_day_checkpoint/script.js
@@ -1,4 +1,9 @@
+let existingList = [];
+
 function addTask(){
     const newItem = document.getElementById("inputItem");
-    return true;
+    existingList.push(newItem);
+    document.getElementById("todoList").innerHTML += `<li>${newItem.value}<li>`;
+    document.getElementById("numberOfTasks").innerHTML = `Total: ${existingList.length} tasks`;
+    return false;
 }
