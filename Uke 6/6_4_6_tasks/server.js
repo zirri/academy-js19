1fd9b91b33e6137482afc542845298d48ec6d46d
zirri <as.borander@gmail.com>
2019-11-22 13:33:47 +0100

    status uke 6

---
+++ Uke 6/6_4_6_tasks/server.js
@@ -0,0 +1,16 @@
+class Grades { 
+    constructor(grade) {
+      this.grade = grade;
+    }
+    
+    getGrade() {
+      return this.grade;
+    }
+
+    setGrade(grade){
+        this.grade = grade;
+    }
+};
+
+const grade = new Grades(5);
+console.log(grade.getGrade())
