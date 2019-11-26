9f37b6d3f2286aa56af9092d594ebc5830916017
zirri <as.borander@gmail.com>
2019-11-26 09:04:10 +0100

    added task 7_1_x (not pushed monday from home

--- Uke 7/7_1_2/server.js
+++ Uke 7/7_1_2/server.js
@@ -1,36 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const fs = require('fs');
 
 // Define middleware
 
-let users = {
-    123:{
-        name:"Anne"
-    }
-}
-
 app.use(bodyParser.json());
+let users = fs.readFileSync('./users.json');
+users = JSON.parse(users)
+function nextId(users){
+    return (Object.keys(users).length)+101;
+}
 
 app.get('/user/:userid', (req, res) => {
     let user = users[req.params.userid];
+    fs.writeFileSync('./users.json', JSON.stringify(users));
     res.json(user)
 })
 
 app.post('/user/', (req, res) => {
-    let user = users[req.params.userid];
-    res.json(user)
+    let name = req.body.name;
+    let gender = req.body.gender;
+    let id = nextId(users);
+    users[id] = {
+        name: name,
+        gender: gender,
+    }
+    fs.writeFileSync('./users.json', JSON.stringify(users));
+    console.log(users)
+    res.sendStatus(204);
 })
 
 app.put('/user/:userid', (req, res) => {
-    let user = users[req.params.userid];
-    res.json(user)
+    let name = req.body.name;
+    let gender = req.body.gender;
+    users[req.params.userid] = {
+        name: name,
+        gender: gender,
+    }
+    fs.writeFileSync('./users.json', JSON.stringify(users));
+    res.sendStatus(204);
 })
 
 app.delete('/user/:userid', (req, res) => {
     delete users[req.params.userid];
+    fs.writeFileSync('./users.json', JSON.stringify(users));
     res.sendStatus(204);
-    console.log(users)
 })
 
 // Listen to port
