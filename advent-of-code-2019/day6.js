8e4630fd4111789b5e017c3e07ea64b76e7efd9d
zirri <as.borander@gmail.com>
2019-12-16 11:02:43 +0100

    status 16. dec

---
+++ advent-of-code-2019/day6.js
@@ -0,0 +1,23 @@
+const input = 'KNO fmw55k8m7i179 z98øyåz8æy67aåy0å6æ7aø1å1438åa5a fmw55k8m7i179 95';
+
+const arrayInput = input.split('');
+for(const i=0; i<arrayInput.length; i++){
+    const letter = arrayInput[i];
+    switch(letter){
+        case '5': 
+            letter = 's';
+            break;
+        case '7': 
+            letter = 't';
+            break;
+        case '3': 
+            letter = 'e';
+            break;
+        case '0': 
+            letter = 'o';
+            break;
+    }
+
+    const 
+}
+
